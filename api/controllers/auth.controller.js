1343a45827dc3fc88541ecd9aa3888f263a0f8ae
Atheeque <atheequemf2003@gmail.com>
2024-12-31 20:57:08 +0530

    added signin and validating a user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import {errorHandler} from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res ,next) => {
 
@@ -18,3 +19,30 @@ export const signup = async (req, res ,next) => {
     }
     //201 something is created
 };
+
+//sign in 
+
+export const signin = async (req, res ,next) => {
+
+    const { username, password } = req.body;
+    try{
+        const validUser = await User.findOne( { username } );
+        if(!validUser) return next(errorHandler(404, "User not found"));
+        // comparing by decrypting the password
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(404, "Password is Wrong!"));
+        const token = jwt.sign({ id: validUser._id }, process.env.SECRET_KEY);
+        //removing the password from the response
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        res.cookie("access_token", token, { httpOnly: true  })
+        .status(200)
+        .json({ rest});
+
+
+    }
+    catch(error){
+        next(error);
+    }
+
+
+}
