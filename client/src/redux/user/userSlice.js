b425685504e53d41c637eeb1a8e747eaa6b69533
Atheeque <atheequemf2003@gmail.com>
2024-12-31 22:58:57 +0530

    add redux

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+
+//initial state
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+};
+
+const userSlice = createSlice({
+
+    name: 'user',
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        signInSuccess: (state, action) => {
+            state.loading = false;
+            state.currentUser = action.payload;
+            state.error = null;
+        },
+        signInFail: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+    },
+});
+
+export const { signInStart, signInSuccess, signInFail } = userSlice.actions;
+
+export default userSlice.reducer;
