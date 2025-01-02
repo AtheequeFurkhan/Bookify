2709673cbb205fd0081173c690a3219c78cb4dd1
Atheeque <atheequemf2003@gmail.com>
2025-01-03 00:00:03 +0530

    fixed errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -75,7 +75,7 @@ export const google = async (req, res ,next) => {
 
                 res.cookie("access_token", token, { httpOnly: true  })
                 .status(200)
-                .json({ best});
+                .json({ rest});
 
             }
 
