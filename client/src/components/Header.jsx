1cab00a4b2d0b60c94d478d7351600a3224f2417
Atheeque <atheequemf2003@gmail.com>
2025-01-03 22:28:20 +0530

    refactor profile page layout and enhance form inputs for better user experience

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,9 +24,9 @@ export default function Header() {
                     <Link to='/about'>
                         <li className='hidden sm:inline text-slate-600 hover:underline'>About</li>
                     </Link>
+                    
                     <Link to='/profile'>
-      
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+                    <li className=' text-slate-700 hover:underline'> Sign in</li>
 
           </Link>
                 </ul>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,13 +4,19 @@ export default function Profile() {
   const currentUser = useSelector((state) => state.user);
 
   return (
-    <div >
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>
       Profile
         </h1>
-    <form>
-      <img src={currentUser.avatar} />
-      <input type="text" placeholder="Username" className="block w-full px-4 py-2 border rounded-md my-4" />
+    <form className="flex flex-col items-center gap-4">
+      {/* <img src={currentUser.user} /> */}
+      <input type="text" placeholder="username" className="w-80 border p-3 rounded-xl "/>
+      <input type="email" placeholder="email" className="w-80 border p-3 rounded-xl "/>
+      <input type="password" placeholder="password" className="w-80 border p-3 rounded-xl "/>
+      <button className = "w-80 border p-3 rounded-xl bg-green-400 text-slate-950 uppercase hover:bg-opacity-75 disabled:85" >Update Profile</button>
+
+      <p></p>
+
 
       
     </form>
