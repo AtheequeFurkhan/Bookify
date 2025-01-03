3d1e23ad8b71b390364baebd4e7c3405a9667d15
Atheeque <atheequemf2003@gmail.com>
2025-01-03 12:05:35 +0530

    enhance profile page with user avatar display and improved layout

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 export default function Profile() {
+  const currentUser = useSelector((state) => state.user);
+
   return (
-    <div>Profile</div>
+    <div >
+      <h1 className='text-3xl font-semibold text-center my-7'>
+      Profile
+        </h1>
+    <form>
+      <img src={currentUser.avatar} />
+      <input type="text" placeholder="Username" className="block w-full px-4 py-2 border rounded-md my-4" />
+
+      
+    </form>
+    </div>
   )
 }
