bba1f19ab26b220c7ccdabc795f3337d5ff94224
Atheeque <atheequemf2003@gmail.com>
2025-01-19 00:20:42 +0530

    added components and updated

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,91 +1,85 @@
-import { useSelector } from "react-redux"
-import { useRef, useState } from "react";
-import { updateUserSuccess , updateUserFailure , updateUserStart } from "../redux/user/userSlice.js";
-import { useDispatch } from "react-redux";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
-export default function Profile() {
-  const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+export default function SignUp() {
   const [formData, setFormData] = useState({});
-  const dispatch = useDispatch();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
-
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
-    })
+      [e.target.id]: e.target.value,
+    });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
     try {
-      dispatch(updateUserStart());
-      const res = await fetch(`/api/user/update/${currentUser.rest._id}`, {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
-  
       const data = await res.json();
+      console.log(data);
       if (data.success === false) {
-        dispatch(updateUserFailure(data.message));
+        setLoading(false);
+        setError(data.message);
         return;
       }
-      dispatch(updateUserSuccess(data));
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
     } catch (error) {
-      dispatch(updateUserFailure(error.message));
+      setLoading(false);
+      setError(error.message);
     }
   };
-  
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className='text-3xl font-semibold text-center my-7'>
-        Profile
-      </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
-         Firebase plan need to be updated 3.45 *
-        <input type="file" ref={fileRef} hidden accept="image/*"/>
-        <img onClick={() => fileRef.current.click()}
-          src={currentUser?.rest?.avatar || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'} 
-          alt="profile" 
-          className="rounded-full h-24 w-24 object-cover cursor-pointer"
-          referrerPolicy="no-referrer"
-        />
-        <input 
-          type="text" 
-          name="username"
-          placeholder="username" 
-          defaultValue={currentUser?.rest?.username}
-          className="w-80 border p-3 rounded-xl"
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
           onChange={handleChange}
         />
-        <input 
-          type="email" 
-          name="email"
-          placeholder="email" 
-          defaultValue={currentUser?.rest?.email}
-          className="w-80 border p-3 rounded-xl"
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
           onChange={handleChange}
         />
-        <input 
-          type="password" 
-          name="password"
-          placeholder="Password" 
-          className="w-80 border p-3 rounded-xl"
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
         />
-        <button className="w-80 border p-3 rounded-xl bg-green-400 text-slate-950 uppercase hover:bg-opacity-75 disabled:opacity-85">
-          Update Profile
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
+        <OAuth/>
       </form>
-
-      <div className="flex justify-between mt-5">
-        <span className="text-red-500 cursor-pointer"> Delete Account </span>
-        <span className="text-red-500 cursor-pointer"> Sign Out </span>
-
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
