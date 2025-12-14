3576639615d188721ff1407fa90340976c365f78
Atheeque <atheequemf2003@gmail.com>
2025-12-15 00:42:25 +0530

    implement user and listing management features; add user deletion, sign out, and listing retrieval functionality

--- api/route/user.route.js
+++ api/route/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { test, updateUser, deleteUser, signOut, getUserListings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verify.user.js";
 
 const router = express.Router();
@@ -11,5 +11,8 @@ const router = express.Router();
 
 router.get('/test',test);
 router.post('/update/:id', verifyToken, updateUser);
+router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/signout', signOut);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -36,10 +36,47 @@ const userSlice = createSlice({
         updateUserFailure: (state, action) => {
             state.loading = false;
             state.error = action.payload;
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        signOutUserStart: (state) => {
+            state.loading = true;
+        },
+        signOutUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        signOutUserFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
         }
     }
 });
 
-export const { signInStart, signInSuccess, signInFail ,updateUserFailure , updateUserStart , updateUserSuccess} = userSlice.actions;
+export const { 
+    signInStart, 
+    signInSuccess, 
+    signInFail,
+    updateUserFailure, 
+    updateUserStart, 
+    updateUserSuccess,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFailure,
+    signOutUserStart,
+    signOutUserSuccess,
+    signOutUserFailure
+} = userSlice.actions;
 
 export default userSlice.reducer;
