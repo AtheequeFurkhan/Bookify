6eaf37f33e55f48e0ecb682006d23b04670cbe46
Atheeque <atheequemf2003@gmail.com>
2025-12-15 00:43:20 +0530

    add swiper dependency and prop-types to ListingItem component

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { MdLocationOn } from 'react-icons/md';
+import PropTypes from 'prop-types';
 
 export default function ListingItem({ listing }) {
   return (
@@ -50,3 +51,19 @@ export default function ListingItem({ listing }) {
     </div>
   );
 }
+
+ListingItem.propTypes = {
+  listing: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    imageUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+    name: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    offer: PropTypes.bool.isRequired,
+    discountPrice: PropTypes.number,
+    regularPrice: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    bedrooms: PropTypes.number.isRequired,
+    bathrooms: PropTypes.number.isRequired,
+  }).isRequired,
+};
