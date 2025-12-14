0d6bc075b0f0a1ea650e76ac73655308c75f375e
Atheeque <atheequemf2003@gmail.com>
2025-12-15 00:46:01 +0530

    refactor Home component: improve error logging and update text for clarity and branding

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        log(error);
+        console.log(error);
       }
     };
     fetchOfferListings();
@@ -50,15 +50,14 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Find your next <span className='text-slate-500'>amazing</span>
           <br />
-          place with ease
+          booking with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
-          live.
+          Bookify is the best place to find and book tickets for events, hotels, and more.
           <br />
-          We have a wide range of properties for you to choose from.
+          We have a wide range of options for you to choose from.
         </div>
         <Link
           to={'/search'}
